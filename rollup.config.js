3a80c474a8e2285b6a4f78d726a8976c1e9c04b8
choi2601 <byeonghyeonchoe55@gmail.com>
2023-01-30 20:55:22 +0900

    typescript 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import babel from "@rollup/plugin-babel";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "./src/index.js",
+  input: "./src/index.ts",
   output: {
     file: "./dist/bundle.js",
     format: "es",
@@ -10,7 +11,13 @@ export default {
   plugins: [
     babel({
       babelHelpers: "bundled",
-      presets: ["@babel/preset-env", "@babel/preset-react"],
+      presets: [
+        "@babel/preset-env",
+        "@babel/preset-react",
+        "@babel/preset-typescript",
+      ],
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
     }),
+    typescript(),
   ],
 };
